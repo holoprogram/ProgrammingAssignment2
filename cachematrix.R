5e993482008feaed84bc6bbfda9e31fd686e3460
holoprogram <2488070031@qq.com>
2022-07-27 15:18:53 +0800

    getting ready to push cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -4,10 +4,12 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
+        m <- NULL  # Cache the values of the inverse matrix
         set <- function(y){
                 x <<- y
-                m <<- NULL 
+                m <<- NULL
+                # use `<<-` to assign a value to an object in an environment 
+                # different from the current environment. 
         }
         get <- function() x
         
@@ -23,12 +25,20 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m <- x$getinverse()
+        # if the inverse has already been calculated
         if(!is.null(m)){
+                # get it from the cache and skips the computation. 
                 message("getting cached data")
                 return(m)
         }
+        
+        # otherwise, calculates the inverse 
         data <- x$get()
         m <- solve(data, ...)
+        
+        # sets the value of the inverse in the cache via the setinv function.
         x$setinverse(m)
+        
         return(m)
+        
 }
